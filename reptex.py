0741c24e635e69aecb3414b762028a26ca4c46e1
Oskar <>
2022-11-24 20:13:31 +0100

    change project's tree

--- reptex.py
+++ reptex.py
@@ -1,14 +1,10 @@
+from src.convert_phrase import convert_phrase
+
 print('Welcome in RepTeX!')
 
 print('1. Simple phrase')
 print('2. File Convert (soon)')
 choice = input("\nWhich option You'd like to choose?")
-
-def convert_phrase(text):
-    text = text.replace('ą', '\k{a}')
-    text = text.replace('ę', '\k{e}')
-
-    print(text)
 
 match(choice):
 
