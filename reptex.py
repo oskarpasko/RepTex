9eebc1d5a2b51ab80f23f03eeab90d97ef7bab86
Oskar <>
2022-11-24 20:10:38 +0100

    Update reptex.py

--- reptex.py
+++ reptex.py
@@ -1,4 +1,20 @@
-from xml.etree.ElementInclude import include
+print('Welcome in RepTeX!')
 
+print('1. Simple phrase')
+print('2. File Convert (soon)')
+choice = input("\nWhich option You'd like to choose?")
 
-text = include('Enter Your phrase: ')
+def convert_phrase(text):
+    text = text.replace('ą', '\k{a}')
+    text = text.replace('ę', '\k{e}')
+
+    print(text)
+
+match(choice):
+
+    case '1':
+        txt = input('Enter Your phrase: ')
+        convert_phrase(txt)
+    case _:
+        print('Wrong choice.')
+
